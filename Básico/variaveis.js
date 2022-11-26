6f8c585dcdf3c06cca621dac7eedfcb8712da84c
DanielWPitsch <102161140+DanielWPitsch@users.noreply.github.com>
2022-11-26 09:15:43 -0300

    up1

---
+++ Básico/variaveis.js
@@ -0,0 +1,15 @@
+//node
+
+console.log("variaveis:")
+
+var nome_dog = "Piloto"
+console.log("O nome do meu cachorro é: " + nome_dog)
+
+var nome = "Daniel"
+console.log("Nome: " + nome)
+
+var idade = 35
+console.log("Nome: " + idade)
+
+var acessou = true
+console.log("Statud do site: " + acessou)
