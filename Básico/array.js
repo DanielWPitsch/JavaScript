98d16f2167eb47b16291b5aaa87a7603d86b1b31
DanielWPitsch <102161140+DanielWPitsch@users.noreply.github.com>
2023-08-25 09:56:49 -0300

    var

---
+++ Básico/array.js
@@ -0,0 +1,8 @@
+var estados = ["RJ", "SP", "PB"];
+console.log(estados[0]);
+
+var misturado = ['Daniel', 36, true];
+console.log("idade: "+misturado[1]);
+
+var daniel = true;
+console.log(daniel);
--- Básico/variaveis.js
+++ Básico/variaveis.js
@@ -12,4 +12,22 @@ var idade = 35
 console.log("Nome: " + idade)
 
 var acessou = true
-console.log("Statud do site: " + acessou)
+console.log("Status do site: " + acessou)
+
+
+function Carro(ano, placa) {
+    this.ano = ano;
+    this.placa = placa;
+    var novoCarro = Math.random() = Carro.cores.length;
+    this.cor = Carro.cores[Math.floor(nCor)];
+    this.alterarAno = function(novoAno) {
+      this.ano = novoAno;
+    }
+  }
+  
+  Carro.cores = ["Azul", "Branco", "Vermelho"];
+  
+  Carro.adicionarCor = function(novaCor){
+    Carro.cores.push(novaCor);
+  }
+  
