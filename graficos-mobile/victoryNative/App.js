dfa6e058bc8ee31bb8409e771999c24aa3ecf522
DanielWPitsch <102161140+DanielWPitsch@users.noreply.github.com>
2024-06-04 13:49:12 -0300

    Victory Native

---
+++ graficos-mobile/victoryNative/App.js
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { CartesianChart, Line } from "victory-native";
+
+const DATA = Array.from({ length: 31 }, (_, i) => ({
+  day: i,
+  highTmp: 40 + 30 * Math.random(),
+}));
+
+export default function App() {
+  return (
+    <View style={{ height: 300 }}>
+      <CartesianChart data={DATA} xKey="day" yKeys={["highTmp"]}>
+        {/* 👇 render function exposes various data, such as points. */}
+        {({ points }) => (
+          // 👇 and we'll use the Line component to render a line path.
+          <Line points={points.highTmp} color="red" strokeWidth={3} />
+        )}
+      </CartesianChart>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
